863942184c1b98c26a6d9dc43ab6bd10e2434e3f
Felicien Nezzi <fnezzi@yahoo.com>
2019-06-06 10:22:56 +0200

    Ajout username et password ce 06/06/2019

--- Connections/conproto.php
+++ Connections/conproto.php
@@ -4,8 +4,9 @@
 # HTTP="true"
 $hostname_conproto = "localhost";
 $database_conproto = "cibase";
-$username_conproto = "";
-$password_conproto = "";
+$username_conproto = "cibase";
+$password_conproto = "fnz@cibase";
 // J'ai enlevé les mots de passe pour question de sécurité
+// modification du 06/06/2019  nouveaux username et mot de passe ajouté
 $conproto = mysql_pconnect($hostname_conproto, $username_conproto, $password_conproto) or die(mysql_error());
 ?>
